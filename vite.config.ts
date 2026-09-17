f9f1814797f25e6a98d8af25885c4c0031fa5378
r1na101 <flokcha@mail.ru>
2026-09-17 15:20:18 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: '/',
+    base: '/r1na101.github.io/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
