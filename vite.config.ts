5f56d1b83db49fde44be88ab1bcad153ecc695de
r1na101 <flokcha@mail.ru>
2026-09-17 19:29:39 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -95,7 +95,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "Умный навигатор для абитуриентов"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
